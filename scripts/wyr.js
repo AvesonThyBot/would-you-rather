b1cbf437c56b0ecd23a2d26f8b92f03122f9d9ea
Aveson <avesoncarvalho25@gmail.com>
2023-11-04 11:50:22 +0000

    removing either-wyr api, doesnt work anymore

--- scripts/wyr.js
+++ scripts/wyr.js
@@ -1,4 +1,1 @@
-const WouldYouRather = require("either-wyr");
-
-WouldYouRather({ count: 3 }).then(console.log); // [Question, Question, Question]
-WouldYouRather().then(console.log); // [Question]
+// finding new api
