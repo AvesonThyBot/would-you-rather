6ea40290ddacccfa9e8fe12b264f6a2ac34bfbc7
Aveson <avesoncarvalho25@gmail.com>
2023-11-04 11:21:04 +0000

    added npm files & added gitignore

---
+++ scripts/wyr.js
@@ -0,0 +1,5 @@
+// Require the either-wyr package
+const WouldYouRather = require("either-wyr");
+
+WouldYouRather({ count: 3 }).then(console.log); // [Question, Question, Question]
+WouldYouRather().then(console.log); // [Question]
